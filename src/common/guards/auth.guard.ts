d5ac378a4ba591d0211e8fbd3657af168ce875a0
ivy.hsieh <ivy.hsieh@jgbsmart.com>
2024-01-18 16:29:26 +0800

    Add: auth guard

--- src/common/guards/auth.guard.ts
+++ src/common/guards/auth.guard.ts
@@ -1,8 +1,39 @@
-import { CanActivate, ExecutionContext } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
+import { UnauthorizedException } from 'src/common/exceptions/custom.exception';
+import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
 
+@Injectable()
 export class AuthGuard implements CanActivate {
-    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+    constructor(
+        private jwtService: JwtService,
+        private configService: ConfigService,
+        private reflector: Reflector,
+    ) {}
+
+    async canActivate(context: ExecutionContext): Promise<boolean> {
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [context.getHandler(), context.getClass()]);
+        if (isPublic) return true;
+
+        const request = context.switchToHttp().getRequest();
+        const token = this.extractTokenFromHeader(request);
+        if (!token) throw new UnauthorizedException();
+
+        try {
+            const payload = await this.jwtService.verifyAsync(token, { secret: this.configService.get<string>('JWT_SECRET') });
+
+            request['user'] = payload;
+        } catch (error) {
+            throw new UnauthorizedException();
+        }
         return true;
+    }
+
+    private extractTokenFromHeader(request: Request): string | undefined {
+        const [type, token] = request.headers.authorization?.split(' ') ?? [];
+        return type === 'Bearer' ? token : undefined;
     }
 }
