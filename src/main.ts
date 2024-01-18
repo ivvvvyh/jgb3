be4315bb6f08147108214c3c7f8100177d3f1352
ivy.hsieh <ivy.hsieh@jgbsmart.com>
2024-01-18 16:31:12 +0800

    Add: API document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from 'src/modules/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    setupSwagger(app);
+    await app.listen(3000);
 }
+
+function setupSwagger(app: INestApplication) {
+    const builder = new DocumentBuilder();
+    const config = builder.setTitle('JGB 3.0 API Documents').setDescription('This is a basic Swagger document.').setVersion('1.0').build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+}
+
 bootstrap();
