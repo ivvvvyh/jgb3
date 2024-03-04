8d642d6c1745fe9db82b3ef6d26725c298773ae4
ivy.hsieh <ivy.hsieh@jgbsmart.com>
2024-03-04 10:55:11 +0800

    Add: estate enums

---
+++ src/common/enums/estate.enum.ts
@@ -0,0 +1,24 @@
+export enum USAGE {
+    UNDEFINED,
+    RESIDENTIAL,
+    BUSINESS,
+    PARKING_SPACE,
+}
+
+// 建築類型
+export enum BUILDING_TYPE {
+    UNDEFINED, // 未定義
+    CONDO, // 大樓
+    APARTMENT, // 公寓
+    TOWNHOUSE, // 透天厝
+    VILLA, // 別墅
+    CONDOMINIUM, // 華夏
+    FARMHOUSE, // 農舍
+    LAND, //土地
+}
+
+export enum PURPOSE_KEY {
+    GENERAL_HOUSING = 1, // 一般住宅
+    SOCIAL_HOUSING = 2, // 社會住宅
+    SHARE_HOUSING = 3, // 社區共用空間
+}
